7e3b7066889b9476b4d3c1aab3d32b5683c7dc38
Kyohei-us <kyohei1212000@gmail.com>
2023-02-01 22:14:35 +0900

    Add login page

--- main.go
+++ main.go
@@ -115,5 +115,9 @@ func main() {
 		})
 	})
 
+	r.GET("/login", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
